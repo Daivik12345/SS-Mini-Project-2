dfb77b4b4e6357995871c824b8037f3b7683343a
Daivik <156230933+Daivik12345@users.noreply.github.com>
2024-05-16 10:36:26 +0530

    Add files via upload

--- Mini project-2 sol.py
+++ Mini project-2 sol.py
@@ -1,9 +1,11 @@
 from itertools import combinations
 
 
-ProductList = {'p1': 10, 'p2': 15, 'p3': 20, 'p4': 25, 'p5': 30, 'p6': 35, 'p7': 50}
+ProductList = {'p1':10, 'p2':15, 'p3':20, 'p4':25, 'p5':30, 'p6':35, 'p7':50,
+               'p8':40, 'p9':55, 'p10':60, 'p11':65, 'p12':75, 'p13':70,
+               'p14':45}
 
-for i in range(1,8):
+for i in range(1,15):
     comb = combinations(ProductList.values(), i)
     
     for item in comb:
